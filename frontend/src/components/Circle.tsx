578a825114698bf68f7dc70ea167145d24ca51dc
ueda masanari <Doraemon123453@gmail.com>
2023-05-27 01:57:11 +0900

    スマホ完成

--- frontend/src/components/Circle.tsx
+++ frontend/src/components/Circle.tsx
@@ -1,38 +1,51 @@
-import React from 'react';
-import styled from 'styled-components';
+import React from 'react'
+import styled from 'styled-components'
+import Image from 'next/image'
+import styles from '@/styles/Circle.module.scss'
 
 type Props = {
-    width: number;
-    height: number;
-};
+  width: number
+  height: number
+  path: string
+  alt: string
+  imgWidth?: number
+  imgHeight?: number
+}
 
 const CircleDiv = styled.div<Props>`
   width: ${(props) => props.width}px;
   height: ${(props) => props.height}px;
-  background: #FFFFFF;
+  background: #ffffff;
   box-shadow: 0px 3px 8px 2px rgba(0, 0, 0, 0.25);
-  border-radius: 70px;
+  border-radius: 100px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
   @media (min-width: 768px) {
     width: ${(props) => props.width * 0.5}px;
-    height: ${(props) => props.height * 0.5}px;
+    height: ${(props) => props.height * 0.5}px;r
   }
 
   @media (min-width: 1024px) {
-    width: ${(props) => props.width * 2}px;
-    height: ${(props) => props.height * 2}px;
+    width: ${(props) => props.width * 1.9}px;
+    height: ${(props) => props.height * 1.9}px;
   }
-`;
+`
 
-const Circle: React.FC<Props> = ({ width, height }) => {
-  return <CircleDiv width={width} height={height} />;
-};
+const Circle: React.FC<Props> = ({
+  width,
+  height,
+  path,
+  alt,
+  imgWidth,
+  imgHeight,
+}) => {
+  return (
+    <CircleDiv width={width} height={height} path={path} alt={alt}>
+        <Image src={path} alt={alt} width={imgWidth ?? width * 0.6} height={imgHeight ?? height * 0.6} />
+    </CircleDiv>
+  )
+}
 
-export default Circle;
-
-
-// export const Circle: FC<Props> = ({ width, height}) => {
-//   return (
-//     <CircleArea width={width} height={height}/>
-//   )
-// }
+export default Circle
